2fdcffb6e3f664a328e446f9fc454d9f633fb545
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-08-25 12:26:51 +0100

    update

---
+++ Balcao/src/main/java/com/mycompany/balcao/Balcao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.balcao;
+
+/**
+ *
+ * @author lucca
+ */
+public class Balcao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
